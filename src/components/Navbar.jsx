0d3ed5f7255e91e07659bdca1e83b55c2fd63b7a
Logan Dietel <logandietel1@gmail.com>
2024-03-22 02:56:53 -0700

    Fix links and background color of hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-import Logo from '../assets/logo.png'
 import { Link } from 'react-scroll'
+import Logo from '../assets/logo.png'
+import resume from '../assets/resume.pdf'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -46,7 +47,9 @@ const Navbar = () => {
             <div onClick={handleClick} className='md:hidden z-10'>
                 {!nav ? <FaBars /> : <FaTimes />}
             </div>
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#EEEDE2] flex flex-col justify-center items-center'}>
+
+            {/* hamburger menu */}
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-gray-50 flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'>
                     <Link onClick={handleClick} to="home" smooth={true} duration={500}>
                         Home
@@ -74,33 +77,40 @@ const Navbar = () => {
                 </li>
             </ul>
 
+            {/* links */}
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                         <a className='flex justify-between items-center w-full text-gray-300' 
-                        href='/'>
+                        href='https://www.linkedin.com/in/logandietel/'
+                        target='_blank'>
                             Linkedin <FaLinkedin size={30}/>
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' 
-                        href='/'>
+                        <a 
+                            className='flex justify-between items-center w-full text-gray-300' 
+                            href='https://github.com/logjscript'
+                            target='_blank'
+                        >
                             Github <FaGithub size={30}/>
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                         <a className='flex justify-between items-center w-full text-gray-300' 
-                        href='/'>
+                            href='mailto:logandietel1@gmail.com'
+                        >
                             Email <HiOutlineMail size={30}/>
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                         <a className='flex justify-between items-center w-full text-gray-300' 
-                        href='/'>
+                            href={resume}
+                            target='_blank'
+                        >
                             Resume <BsFillPersonLinesFill size={30}/>
                         </a>
                     </li>
-                    
                 </ul>
             </div>
         </nav>
